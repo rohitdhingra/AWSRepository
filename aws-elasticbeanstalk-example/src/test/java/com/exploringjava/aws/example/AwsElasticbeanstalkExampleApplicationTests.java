619bd135566f6072fab828435464ec685772c322
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-07-26 13:05:38 +0530

    Elastic Bean Stalk example

---
+++ aws-elasticbeanstalk-example/src/test/java/com/exploringjava/aws/example/AwsElasticbeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploringjava.aws.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticbeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
